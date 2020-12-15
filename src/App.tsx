b26d724f5160630dccad1040e2f58a74b90b3916
Andre Silveira <als.andresilveira@gmail.com>
2020-12-15 20:51:12 -0300

    add eslint config

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
-import {View} from 'react-native';
+import React from "react"
+import {View} from 'react-native'
 
-const App = () => <View />;
+const App = () => <View />
 
-export default App;
+
+export default App
