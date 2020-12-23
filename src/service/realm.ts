2b44aea57296e231bda7db3ec3a4842e5dc7e602
Andre Silveira <als.andresilveira@gmail.com>
2020-12-22 22:23:02 -0300

    add favorite schema in service

--- src/service/realm.ts
+++ src/service/realm.ts
@@ -1,8 +1,9 @@
 import Realm from 'realm';
+import FavoriteSchema from '../schemas/FavoriteSchema';
 import UserSchema from '../schemas/UserSchema';
 
 export default function getRealmApp() {
   return Realm.open({
-    schema: [UserSchema],
+    schema: [UserSchema, FavoriteSchema],
   });
 }
